e52d52e0d2f98dfe0acd60e60de7b08fc7c30379
R. Martell <rjmartell@computer.org>
2016-07-06 15:56:24 -0700

    Add support for command-line arguments; update release notes

--- src/main/java/org/opengis/cite/kml2/TestNGController.java
+++ src/main/java/org/opengis/cite/kml2/TestNGController.java
@@ -3,7 +3,6 @@ package org.opengis.cite.kml2;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +18,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 import com.occamlab.te.spi.executors.TestRunExecutor;
 import com.occamlab.te.spi.executors.testng.TestNGExecutor;
 import com.occamlab.te.spi.jaxrs.TestSuiteController;
@@ -28,128 +29,137 @@ import com.occamlab.te.spi.jaxrs.TestSuiteController;
  */
 public class TestNGController implements TestSuiteController {
 
-	private TestRunExecutor executor;
-	private Properties etsProperties = new Properties();
+    private TestRunExecutor executor;
+    private Properties etsProperties = new Properties();
 
-	/**
-	 * A convenience method to facilitate test development.
-	 *
-	 * @param args
-	 *            Test run arguments (optional). The first argument must refer
-	 *            to an XML properties file containing the expected set of test
-	 *            run arguments. If no argument is supplied, the file located at
-	 *            ${user.home}/test-run-props.xml will be used.
-	 * @throws Exception
-	 *             If the test run cannot be executed (usually due to
-	 *             unsatisfied pre-conditions).
-	 */
-	public static void main(String[] args) throws Exception {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		File xmlArgs = null;
-		if (args.length > 0) {
-			xmlArgs = (args[0].startsWith("file:")) ? new File(
-					URI.create(args[0])) : new File(args[0]);
-		} else {
-			String homeDir = System.getProperty("user.home");
-			xmlArgs = new File(homeDir, "test-run-props.xml");
-		}
-		if (!xmlArgs.exists()) {
-			throw new IllegalArgumentException(
-					"Test run arguments not found at " + xmlArgs);
-		}
-		Document testRunArgs = db.parse(xmlArgs);
-		TestNGController controller = new TestNGController();
-		Source testResults = controller.doTestRun(testRunArgs);
-		System.out.println("Test results: " + testResults.getSystemId());
-	}
+    /**
+     * A convenience method for running the test suite using a command-line
+     * interface. The default values of the test run arguments are as follows:
+     * <ul>
+     * <li>XML properties file: ${user.home}/test-run-props.xml</li>
+     * <li>outputDir: ${user.home}</li>
+     * <li>deleteSubjectOnFinish: false</li>
+     * </ul>
+     * <p>
+     * <strong>Synopsis</strong>
+     * </p>
+     *
+     * <pre>
+     * ets-*-aio.jar [-o|--outputDir $TMPDIR] [-d|--deleteSubjectOnFinish] [test-run-props.xml]
+     * </pre>
+     *
+     * @param args
+     *            Test run arguments (optional). The first argument must refer
+     *            to an XML properties file containing the expected set of test
+     *            run arguments. If no argument is supplied, the file located at
+     *            ${user.home}/test-run-props.xml will be used.
+     * @throws Exception
+     *             If the test run cannot be completed (usually due to
+     *             unsatisfied pre-conditions).
+     */
+    public static void main(String[] args) throws Exception {
+        CommandLineArguments testRunArgs = new CommandLineArguments();
+        JCommander cmd = new JCommander(testRunArgs);
+        try {
+            cmd.parse(args);
+        } catch (ParameterException px) {
+            System.out.println(px.getMessage());
+            cmd.usage();
+        }
+        if (testRunArgs.doDeleteSubjectOnFinish()) {
+            System.setProperty("deleteSubjectOnFinish", "true");
+        }
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        File xmlArgs = testRunArgs.getPropertiesFile();
+        Document testRunProps = db.parse(xmlArgs);
+        TestNGController controller = new TestNGController(testRunArgs.getOutputDir());
+        Source testResults = controller.doTestRun(testRunProps);
+        System.out.println("Test results: " + testResults.getSystemId());
+    }
 
-	/**
-	 * Default constructor uses the location given by the "user.home" system
-	 * property as the root output directory.
-	 */
-	public TestNGController() {
-		this(new File(System.getProperty("user.home")).toURI().toString());
-	}
+    /**
+     * Default constructor uses the location given by the "user.home" system
+     * property as the root output directory.
+     */
+    public TestNGController() {
+        this(new File(System.getProperty("user.home")).toURI().toString());
+    }
 
-	/**
-	 * Construct a controller that writes results to the given output directory.
-	 * 
-	 * @param outputDirUri
-	 *            A file URI that specifies the location of the directory in
-	 *            which test results will be written. It will be created if it
-	 *            does not exist.
-	 */
-	public TestNGController(String outputDirUri) {
-		InputStream is = getClass().getResourceAsStream("ets.properties");
-		try {
-			this.etsProperties.load(is);
-		} catch (IOException ex) {
-			TestSuiteLogger.log(Level.WARNING,
-					"Unable to load ets.properties. " + ex.getMessage());
-		}
-		URL tngSuite = TestNGController.class.getResource("testng.xml");
-		File resultsDir = new File(URI.create(outputDirUri));
-		TestSuiteLogger.log(Level.CONFIG, "Using TestNG config: " + tngSuite);
-		TestSuiteLogger.log(Level.CONFIG,
-				"Using outputDirPath: " + resultsDir.getAbsolutePath());
-		// NOTE: setting third argument to 'true' enables the default listeners
-		this.executor = new TestNGExecutor(tngSuite.toString(),
-				resultsDir.getAbsolutePath(), false);
-	}
+    /**
+     * Construct a controller that writes results to the given output directory.
+     * 
+     * @param outputDir
+     *            The location of the directory in which test results will be
+     *            written. It will be created if it does not exist.
+     */
+    public TestNGController(String outputDir) {
+        InputStream is = getClass().getResourceAsStream("ets.properties");
+        try {
+            this.etsProperties.load(is);
+        } catch (IOException ex) {
+            TestSuiteLogger.log(Level.WARNING, "Unable to load ets.properties. " + ex.getMessage());
+        }
+        URL tngSuite = TestNGController.class.getResource("testng.xml");
+        File resultsDir;
+        if (null == outputDir || outputDir.isEmpty()) {
+            resultsDir = new File(System.getProperty("user.home"));
+        } else {
+            resultsDir = new File(outputDir);
+        }
+        TestSuiteLogger.log(Level.CONFIG, "Using TestNG config: " + tngSuite);
+        TestSuiteLogger.log(Level.CONFIG, "Using outputDirPath: " + resultsDir.getAbsolutePath());
+        // NOTE: setting third argument to 'true' enables the default listeners
+        this.executor = new TestNGExecutor(tngSuite.toString(), resultsDir.getAbsolutePath(), false);
+    }
 
-	@Override
-	public String getCode() {
-		return etsProperties.getProperty("ets-code");
-	}
+    @Override
+    public String getCode() {
+        return etsProperties.getProperty("ets-code");
+    }
 
-	@Override
-	public String getVersion() {
-		return etsProperties.getProperty("ets-version");
-	}
+    @Override
+    public String getVersion() {
+        return etsProperties.getProperty("ets-version");
+    }
 
-	@Override
-	public String getTitle() {
-		return etsProperties.getProperty("ets-title");
-	}
+    @Override
+    public String getTitle() {
+        return etsProperties.getProperty("ets-title");
+    }
 
-	@Override
-	public Source doTestRun(Document testRunArgs) throws Exception {
-		validateTestRunArgs(testRunArgs);
-		return executor.execute(testRunArgs);
-	}
+    @Override
+    public Source doTestRun(Document testRunArgs) throws Exception {
+        validateTestRunArgs(testRunArgs);
+        return executor.execute(testRunArgs);
+    }
 
-	/**
-	 * Validates the test run arguments. The test run is aborted if any of these
-	 * checks fail.
-	 *
-	 * @param testRunArgs
-	 *            A DOM Document containing a set of XML properties (key-value
-	 *            pairs).
-	 * @throws IllegalArgumentException
-	 *             If any arguments are missing or invalid for some reason.
-	 */
-	void validateTestRunArgs(Document testRunArgs)
-			throws IllegalArgumentException {
-		if (null == testRunArgs
-				|| !testRunArgs.getDocumentElement().getNodeName()
-						.equals("properties")) {
-			throw new IllegalArgumentException(
-					"Input is not an XML properties document.");
-		}
-		NodeList entries = testRunArgs.getDocumentElement()
-				.getElementsByTagName("entry");
-		if (entries.getLength() == 0) {
-			throw new IllegalArgumentException("No test run arguments found.");
-		}
-		Map<String, String> args = new HashMap<String, String>();
-		for (int i = 0; i < entries.getLength(); i++) {
-			Element entry = (Element) entries.item(i);
-			args.put(entry.getAttribute("key"), entry.getTextContent());
-		}
-		if (!args.containsKey(TestRunArg.KML.toString())) {
-			throw new IllegalArgumentException(String.format(
-					"Missing argument: '%s' must be present.", TestRunArg.KML));
-		}
-	}
+    /**
+     * Validates the test run arguments. The test run is aborted if any of these
+     * checks fail.
+     *
+     * @param testRunArgs
+     *            A DOM Document containing a set of XML properties (key-value
+     *            pairs).
+     * @throws IllegalArgumentException
+     *             If any arguments are missing or invalid for some reason.
+     */
+    void validateTestRunArgs(Document testRunArgs) throws IllegalArgumentException {
+        if (null == testRunArgs || !testRunArgs.getDocumentElement().getNodeName().equals("properties")) {
+            throw new IllegalArgumentException("Input is not an XML properties document.");
+        }
+        NodeList entries = testRunArgs.getDocumentElement().getElementsByTagName("entry");
+        if (entries.getLength() == 0) {
+            throw new IllegalArgumentException("No test run arguments found.");
+        }
+        Map<String, String> args = new HashMap<String, String>();
+        for (int i = 0; i < entries.getLength(); i++) {
+            Element entry = (Element) entries.item(i);
+            args.put(entry.getAttribute("key"), entry.getTextContent());
+        }
+        if (!args.containsKey(TestRunArg.KML.toString())) {
+            throw new IllegalArgumentException(
+                    String.format("Missing argument: '%s' must be present.", TestRunArg.KML));
+        }
+    }
 }
--- src/test/java/org/opengis/cite/kml2/VerifyTestNGController.java
+++ src/test/java/org/opengis/cite/kml2/VerifyTestNGController.java
@@ -1,6 +1,6 @@
 package org.opengis.cite.kml2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -8,22 +8,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
-import java.util.logging.Level;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
-import javax.xml.transform.dom.DOMSource;
-
-import net.sf.saxon.s9api.XdmValue;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.opengis.cite.kml2.util.TestSuiteLogger;
 import org.opengis.cite.kml2.util.XMLUtils;
 import org.w3c.dom.Document;
+
+import net.sf.saxon.s9api.XdmValue;
 
 /**
  * Verifies the results of executing a test run using the main controller
@@ -40,29 +37,23 @@ public class VerifyTestNGController {
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		dbf.setNamespaceAware(true);
 		dbf.setValidating(false);
-		dbf.setFeature(
-				"http://apache.org/xml/features/nonvalidating/load-external-dtd",
-				false);
+		dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 		docBuilder = dbf.newDocumentBuilder();
 	}
 
 	@Before
-	public void loadDefaultTestRunProperties()
-			throws InvalidPropertiesFormatException, IOException {
+	public void loadDefaultTestRunProperties() throws InvalidPropertiesFormatException, IOException {
 		this.testRunProps = new Properties();
-		this.testRunProps.loadFromXML(getClass().getResourceAsStream(
-				"/test-run-props.xml"));
+		this.testRunProps.loadFromXML(getClass().getResourceAsStream("/test-run-props.xml"));
 	}
 
 	@Test
 	public void doTestRun() throws Exception {
 		URL testSubject = getClass().getResource("/kml22/Placemark-001.xml");
-		this.testRunProps.setProperty(TestRunArg.KML.toString(), testSubject
-				.toURI().toString());
+		this.testRunProps.setProperty(TestRunArg.KML.toString(), testSubject.toURI().toString());
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream(1024);
 		this.testRunProps.storeToXML(outStream, "Integration test");
-		Document testRunArgs = docBuilder.parse(new ByteArrayInputStream(
-				outStream.toByteArray()));
+		Document testRunArgs = docBuilder.parse(new ByteArrayInputStream(outStream.toByteArray()));
 		TestNGController controller = new TestNGController();
 		Source results = controller.doTestRun(testRunArgs);
 		String xpath = "/testng-results/@failed";
